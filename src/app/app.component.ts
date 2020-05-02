f01e6d8441a11d6257c80b59acc387d4ecb5b9c9
Ismael Silva <ismaelgomesesilva@gmail.com>
2020-05-02 12:57:54 +0000

    Firts commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hellow-word';
+  name= "";
+
+  displayHeader=false;
+
+  color = "red";
+
+  colors = ["red", "green", "blue", "purple", "yellow"];
+
+  /* Conversation Father and son*/
+  public nameToChild = "Ismael";
+  public message = "";
+  /* Finish */
+
+  /* Pipe */
+  namePipe = 'Codevolution'
+  messagePipe = 'Welcome to codevolution'
+  person={
+    "firstname" : "Ismael",
+    "lastname" : "Silva"
+  }
+  datePipe = new Date()
+
+  /* Finish */
+
+  /* Service */
+  
+
+
+  /* Finish */
+
+  onClick($values){
+    console.log($values);
+  }
 }
